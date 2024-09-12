5de66b9bb01aa0182c4993b4c21b3d00cfa55603
nguyentaiduc0412 <48790583+nguyentaiduc0412@users.noreply.github.com>
2024-09-12 09:36:03 +0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const VERIFY_TASK_MAPPING = {
+  "38f6dd88-57bd-4b42-8712-286a06dac0a0": "VALUE", // Title: "How to Analyze Crypto?";
+  "6af85c01-f68d-4311-b78a-9cf33ba5b151": "GO GET", // Title: "Forks Explanied"
+};
